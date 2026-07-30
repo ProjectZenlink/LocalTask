2ced71444f08640f41304786005920084db95a30
OmniStack Global <roman@omnistackglobal.com>
2026-07-30 18:50:08 +0800

    Deleted App.tsx

--- src/am/Tasks.tsx
+++
@@ -1,9 +0,0 @@
-import AdminTasks from '../admin/Tasks'
-import { useAm } from './AmLayout'
-
-/** AM 端任务列表 = 控制台同页 + amScope（自动限定自己名下）。 */
-export default function AmTasks() {
-  const { am } = useAm()
-  if (!am) return <p className="text-muted">…</p>
-  return <AdminTasks amScope={am} />
-}
