9cb6c8945b41cadd1904f63ba255bd5063dcf5eb
OmniStack Global <roman@omnistackglobal.com>
2026-08-01 19:58:26 +0800

    Deleted App.tsx

--- src/am/Leads.tsx
+++
@@ -1,8 +0,0 @@
-import LeadsDesk from '../components/LeadsDesk'
-import { useAm } from './AmLayout'
-
-export default function AmLeads() {
-  const { am } = useAm()
-  if (!am) return <p className="text-muted">…</p>
-  return <LeadsDesk isAdmin={false} amId={am.id} />
-}
