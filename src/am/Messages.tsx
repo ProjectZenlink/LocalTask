7ce55a4e4f7f21936fba7ef5c216e103cdf4673f
OmniStack Global <roman@omnistackglobal.com>
2026-08-03 16:23:25 +0800

    Deleted vite-env.d.ts

--- src/am/Messages.tsx
+++
@@ -1,7 +0,0 @@
-import ChatCenter from '../components/ChatCenter'
-import { useLang } from '../admin/i18n'
-
-export default function AmMessages() {
-  const { lang } = useLang()
-  return <ChatCenter lang={lang} myRole="am" />
-}
