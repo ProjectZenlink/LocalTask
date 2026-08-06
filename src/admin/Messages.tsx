90c345d747a87b12865a6d33f27803820f3e839c
OmniStack Global <roman@omnistackglobal.com>
2026-08-06 14:25:09 +0800

    Deleted index.css

--- src/admin/Messages.tsx
+++
@@ -1,7 +0,0 @@
-import ChatCenter from '../components/ChatCenter'
-import { useLang } from './i18n'
-
-export default function AdminMessages() {
-  const { lang } = useLang()
-  return <ChatCenter lang={lang} myRole="admin" />
-}
