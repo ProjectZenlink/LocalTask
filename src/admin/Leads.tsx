470f41f485175f067c5c8239af7f985bfb16f2ff
OmniStack Global <roman@omnistackglobal.com>
2026-08-08 11:41:50 +0800

    Deleted App.tsx

--- src/admin/Leads.tsx
+++
@@ -1,5 +0,0 @@
-import LeadsDesk from '../components/LeadsDesk'
-
-export default function AdminLeads() {
-  return <LeadsDesk isAdmin amId={null} />
-}
--- src/pages/Messages.tsx
+++
@@ -1,7 +0,0 @@
-import ChatCenter from '../components/ChatCenter'
-import { useI18n } from '../lib/i18n'
-
-export default function FreelancerMessages() {
-  const { lang } = useI18n()
-  return <ChatCenter lang={lang} myRole="user" />
-}
